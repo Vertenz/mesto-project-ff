f0857f338355b0523cc32f657121dbc8cda1a04d
Vladimir Vertenz <vladmirworld@gmail.com>
2024-11-28 02:10:16 +0300

    Добавить функции для создания и удаления карточек, а также вывод карточек на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+// Функция создания карточки
+function createCard(cardData, handleDelete) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+    
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+    
+    deleteButton.addEventListener('click', handleDelete);
+    
+    return cardElement;
+}
 
-// @todo: DOM узлы
+// Функция удаления карточки
+function handleDeleteCard(event) {
+    event.target.closest('.card').remove();
+}
 
-// @todo: Функция создания карточки
+// Вывод карточек на страницу
+const placesList = document.querySelector('.places__list');
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, handleDeleteCard);
+    placesList.append(cardElement);
+});
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+placesList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('card__like-button')) {
+        event.target.parentNode.remove();
+    }
+});
