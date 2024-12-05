70d8299542e7c2de22023bae25cb57dd03b92752
Vladimir Vertenz <vladmirworld@gmail.com>
2024-12-06 02:00:34 +0300

    feat: добавить валидацию форм и улучшить структуру HTML для редактирования профиля

---
+++ src/scripts/validate.js
@@ -0,0 +1,9 @@
+const formClasses = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  noErrorClass: '.popup__error_invisible',
+  errorClass: 'popup__error_visible'
+};
